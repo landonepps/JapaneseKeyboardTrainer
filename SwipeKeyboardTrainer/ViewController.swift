0b16b44b5c8c2974d6d1e5961f27e65d1be56b76
Landon Epps <landon.epps@gmail.com>
2019-03-22 22:21:06 +0900

    Initial commit

---
+++ SwipeKeyboardTrainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeKeyboardTrainer
+//
+//  Created by Landon Epps on 3/22/19.
+//  Copyright © 2019 Landon Epps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
